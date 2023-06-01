e85f0e68fcb258f02e2c5fe4d5f4ab99b6351574
Aiub Duduev <aub.duduev.1989@gmail.com>
2023-06-01 17:30:23 +0300

    init

--- Sources/MindboxFramework/HandlerDidTapPushNotificationService.swift
+++ Sources/MindboxFramework/HandlerDidTapPushNotificationService.swift
@@ -6,11 +6,17 @@ final public class HandlerDidTapPushNotificationService {
     
     public var presentAdvertising: ((String) -> Void)?
     
+    public var urlAdvertising: String?
+    
     public func handler(with response: UNNotificationResponse){
         let userInfo = response.notification.request.content.userInfo
         guard let urlAdvertising = parse(with: userInfo) else { return }
         Mindbox.shared.pushClicked(response: response)
-        presentAdvertising?(urlAdvertising)
+        if self.presentAdvertising == nil {
+            self.urlAdvertising = urlAdvertising
+        } else {
+            self.presentAdvertising?(urlAdvertising)
+        }
     }
     
     private func parse(with userInfo: [AnyHashable : Any]) -> String? {
