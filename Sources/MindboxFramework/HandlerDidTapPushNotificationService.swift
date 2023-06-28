5cdf1b3306a954d03e86bc5e13a2816f21f022df
Аюб Дудуев <aub.duduev.1989@gmail.com>
2023-06-28 12:20:58 +0300

    правки

--- Sources/MindboxFramework/HandlerDidTapPushNotificationService.swift
+++ Sources/MindboxFramework/HandlerDidTapPushNotificationService.swift
@@ -4,25 +4,44 @@ import UserNotifications
 
 final public class HandlerDidTapPushNotificationService {
     
-    public var presentAdvertising: ((String) -> Void)?
+    public var presentAdvertising: ((PushModel) -> Void)?
     
-    public var urlAdvertising: String?
+    public var pushModel: PushModel?
     
     public func handler(with response: UNNotificationResponse){
         let userInfo = response.notification.request.content.userInfo
-        guard let urlAdvertising = parse(with: userInfo) else { return }
+        guard let pushModel = parse(with: userInfo) else { return }
         Mindbox.shared.pushClicked(response: response)
         if self.presentAdvertising == nil {
-            self.urlAdvertising = urlAdvertising
+            self.pushModel = pushModel
         } else {
-            self.presentAdvertising?(urlAdvertising)
+            self.presentAdvertising?(pushModel)
         }
     }
     
-    private func parse(with userInfo: [AnyHashable : Any]) -> String? {
-        let domainAdvertising = userInfo["clickUrl"] as? String
-        return domainAdvertising
+    private func parse(with userInfo: [AnyHashable : Any]) -> PushModel? {
+        let clickUrlString = (userInfo["clickUrl"] as? String) ?? ""
+        let homeUrlString = (userInfo["homeUrl"] as? String) ?? ""
+        let clickUrl = URL(string: clickUrlString)
+        let homeUrl = URL(string: homeUrlString)
+        let pushModel = PushModel(
+            homeUrl: homeUrl,
+            clickUrl: clickUrl
+        )
+        return pushModel
     }
     
+    public struct PushModel {
+        public let homeUrl: URL?
+        public let clickUrl: URL?
+        
+        public init(
+            homeUrl: URL?,
+            clickUrl: URL?
+        ) {
+            self.homeUrl = homeUrl
+            self.clickUrl = clickUrl
+        }
+    }
     public init() {}
 }
